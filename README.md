b82c95623935986e4a8774fe1d6020772951a448
Van Gogh <139753682+CircuitSorcerer@users.noreply.github.com>
2023-09-07 23:58:03 +1000

    More Javascript functionality, css screen response.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Password Generator Website
+
+I have made this site to generate passwords with custom filters to make picking a password easy.
--- assets/script.js
+++ assets/script.js
@@ -8,11 +8,11 @@ function getParams() {
     options = {};
     // Functions for getting and filtering user input
     function getLength() {
-        var length = parseInt(prompt("Length  of password?", "between 8 and 128"))
-        console.log(length)
+        var length = parseInt(prompt("Length  of password?", "between 8 and 128"));
+        console.log(length);
         if (isNaN(length) || length < 8 || length > 128) {
-            alert("Invalid Entry, please choose an integer between 8 and 128")
-            getLength()
+            alert("Invalid Entry, please choose an integer between 8 and 128");
+            getLength();
         }
         return length;
     }
@@ -77,12 +77,19 @@ function getParams() {
     nums = getNums();
     symbols = getSymbols();
 
-    // Print to console for debugging
+    //Check at least one input has been chosen
+    if (options.lowerCase === false && options.upperCase === false && options.nums === false && options.symbols === false ) {
+        alert("Must select at least one character option!")
+        displayPassword(passwordGenerator(getParams()));
+    } else {
+        console.log("NO WORKY")
+    }
     
 
     return options;
 }
 
+// Function to take user inputs and generate a random string
 function passwordGenerator(options) {
     console.log(options);
     var useChar = []
@@ -100,8 +107,9 @@ function passwordGenerator(options) {
         useChar.push(symbolList)
     }
     console.log(useChar);
-    for (i=0; i<options.length; i++) {
-        var randList1 = useChar[Math.floor(Math.random()*useChar.length)];
+    for (var i=0; i<options.length; i++) {
+        var randList1 = [];
+        randList1 = useChar[Math.floor(Math.random()*useChar.length)];
         console.log(randList1);
         var randListChar = randList1[Math.floor(Math.random()*randList1.length)];
         console.log(randListChar);
@@ -109,25 +117,84 @@ function passwordGenerator(options) {
     }
     console.log(passString);
     console.log(passString.length);
+
+    /*    PASSWORD FILTERING TO BE REVISITED
+    for (i=0; i<useChar.length; i++) {
+        j = 0;
+        for (j=0; j<passString.length; j++) {
+            console.log(useChar[i]);
+            console.log()
+        if (useChar[i].includes(passString[j]) == false) {
+            contained = false;
+        } else {
+            return contained = true;
+        }
+        console.log(contained)
+        }
+        if (contained === false) {
+            return displayPassword(passwordGenerator(options));
+        }
+    }
+    var contained = false;
+    var i = 0;
+    while (i < useChar.length) {
+        let j= 0;
+        do {
+            if (useChar[i].includes(passString[j]) == false) {
+                contained = false;
+                j++;
+            } else {
+                return contained = true;
+            }
+            console.log(contained)
+        } while (j < passString.length || contained === false);
+        if (contained === false) {
+            console.log("Success")
+            return displayPassword(passwordGenerator(options));
+        }
+    }*/
+
     return passString;
 }
 
+// Display the generated password in the box on screen
 var passwordBox = document.querySelector("#password-box")
 
 function displayPassword(passString) {
     passwordBox.innerHTML = passString;
 }
 
-displayPassword(passwordGenerator(getParams()));
 
+// Button code, changes from generate to regenerate and detects click
 var button = document.querySelector("button")
 
+function buttonText() {
+    if (webpageFresh === false) {
+        button.textContent = "Regenerate"
+    }
+}
+
 button.addEventListener("click", function(){
-    displayPassword(passwordGenerator(options))    
+    if (webpageFresh === true) {
+        webpageFresh = false
+        displayPassword(passwordGenerator(getParams()));
+        buttonText()
+    } else {
+        displayPassword(passwordGenerator(options))
+    }
 })
 
-passwordBox.addEventListener("click", function() {
+
+/*   CLICK TO COPY TO BE REVISITED
+passwordBox.addEventListener("click", function clickToCopy() {
     const passBoxText = document.getElementById("passText");
     passBoxText.select();
-    document.execCommand("copy");
-})
+    navigator.clipboard.writeText(passBoxText.value);
+})*/
+
+// Initialisation function
+function init() {
+    webpageFresh = true;
+}
+
+init();
